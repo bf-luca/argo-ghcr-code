01e1d9f8df40a568308c34880a44be8d5373c204
Luca Dreiling <luca.dreiling@bergfreunde.de>
2023-12-04 14:58:24 +0100

    Change app

--- helloserver/server.go
+++ helloserver/server.go
@@ -67,8 +67,7 @@ func version(w http.ResponseWriter, r *http.Request) {
 func greet(w http.ResponseWriter, r *http.Request) {
 	name := strings.Trim(r.URL.Path, "/")
 	if name == "" {
-		name = "Gopher",
-		name = "Looper"
+		name = "Gopher"
 	}
 
 	fmt.Fprintf(w, "<!DOCTYPE html>\n")
